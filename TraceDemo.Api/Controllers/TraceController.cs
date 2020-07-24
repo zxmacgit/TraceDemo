11c2bbce25d3bbc2f4b4fff5ea4a63074b5718b0
zxmacgit <61131110+zxmacgit@users.noreply.github.com>
2020-07-24 21:48:19 +0800

    Backend and frontend updates

--- TraceDemo.Api/Controllers/TraceController.cs
+++ TraceDemo.Api/Controllers/TraceController.cs
@@ -1,10 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using System.Collections.Generic;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
-using RestSharp;
 
 namespace TraceDemo.Api.Controllers
 {
@@ -18,15 +15,6 @@ namespace TraceDemo.Api.Controllers
         [Produces("application/json")]
         public async Task<IActionResult> Get(double lat, double lon)
         {
-            // var result = await _mediator.Send(new GetAllTierCommand());
-            //var client = new RestClient("https://api.openweathermap.org/data/2.5/");
-            //var request = new RestRequest("onecall")
-            //    .AddParameter("lat", lat)
-            //    .AddParameter("lon", lon)
-            //    //.AddParameter("exclude", null)
-            //    .AddParameter("appid", "b001e24ecf2722223d18c77159f60bb0");
-            //var response = await client.GetAsync<object>(request);
-
             var result = await new RestWrapper("https://api.openweathermap.org/data/2.5/")
                 .Resource("onecall")
                 .AddParam("lat", lat)
@@ -43,13 +31,8 @@ namespace TraceDemo.Api.Controllers
         [Produces("application/json")]
         public async Task<IActionResult> Locate(string ip)
         {
-            // var result = await _mediator.Send(new GetAllTierCommand());
-            // var client = new RestClient("http://api.ipstack.com/" + ip);
-            //var client = new RestClient("http://ip-api.com/json/" + ip);
-            //var request = new RestRequest()
-            //    .AddParameter("access_key", "66a0f82b63ae8175ce47f03ab2052256");
-            //var response = await client.GetAsync<object>(request);
-            var result = await new RestWrapper("http://ip-api.com/json/" + ip)
+            var result = await new RestWrapper("http://ip-api.com/json/")
+                .Resource(ip)
                 .AddParam("access_key", "66a0f82b63ae8175ce47f03ab2052256")
                 .GetAsync<object>();
 
--- TraceDemo.WebApp/wwwroot/js/site.js
+++ TraceDemo.WebApp/wwwroot/js/site.js
@@ -1,14 +1,5 @@
 ﻿$(function () {
-    //$.ajax({
-    //    method: "GET",
-    //    url: "http://api.ipstack.com/49.145.205.173?access_key=66a0f82b63ae8175ce47f03ab2052256",
-    //})
-    //    .done(function (data) {
-    //        callback(data);
-    //    })
-    //    .fail(function (data) {
-    //        console.log('fail: ', data);
-    //    });
+    const backendApi = "https://localhost:44313";
 
     function getGeoIpLookUp(callback) {
         $.ajax({
@@ -26,7 +17,21 @@
     function getWeather(lat, lon, callback) {
         $.ajax({
             method: "GET",
-            url: "https://localhost:44313/api/trace/weather?" + "lat=" + lat + "&lon=" + lon,
+            url: `${backendApi}/api/trace/weather?lat=${lat}&lon=${lon}`,
+        })
+        .done(function (data) {
+            callback(data);
+        })
+        .fail(function (data) {
+            console.log('fail: ', data);
+            $('#loader').hide();
+        });
+    }
+
+    function getGeoByIp(ip, callback) {
+        $.ajax({
+            method: "GET",
+            url: `${backendApi}/api/trace/locate?ip=${ip}`,
         })
         .done(function (data) {
             callback(data);
@@ -36,7 +41,7 @@
         });
     }
     
-    function loadAll() {
+    function loadInit() {
         getGeoIpLookUp((geoIpLookUp) => {
             console.log('geoIpLookUp: ', geoIpLookUp);
             if (!geoIpLookUp) return;
@@ -44,33 +49,91 @@
             let lon = geoIpLookUp.longitude
             getWeather(lat, lon, (weather) => {
                 console.log('weather: ', weather);
-                buildContent(weather);
+                buildContent(geoIpLookUp, weather);
             });
         });
     }
 
-    function buildContent(weather) {
+    function loadByIp(ip) {
+        $('#loader').show();
+        console.log(ip);
+        getGeoByIp(ip, (geo) => {
+            console.log('getGeoByIp: ', geo);
+            if (!geo) return;
+            let lat = geo.lat;
+            let lon = geo.lon
+            getWeather(lat, lon, (weather) => {
+                console.log('weather: ', weather);
+                buildContent(geo, weather);
+                $('#loader').hide();
+            });
+        });
+    }
+
+    function buildContent(geo, weather) {
+        $('#currentWeather1').children().remove();
+        $('#currentWeather2').children().remove();
+        $('#dailyWeather').children().remove();
+
+        const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
         let current = weather.current;
         let daily = weather.daily;
-        let currentWeather = '';
-        currentWeather += '<p class="cloudy"> ' + current.clouds + '</p >';
-        currentWeather += '<image src>';
+        let date = new Date();
+        let time = `${days[date.getDay()]} ${(date.getHours() > 12 ? date.getHours() - 12 : date.getHours())}:${date.getMinutes()}`;
+        let createElem = (el, data, attr = '') => `<${el}${attr}>${data}</${el}>`;
+        let combineElems = (elems) => elems.join('');
+        let conv = (valNum) => (parseFloat(valNum) - 273.15).toFixed();
+
+        let currentWeather1 = combineElems([
+            createElem('h3', `${geo.city}, ${geo.regionName}`),
+            createElem('p', time),
+            `<i class="${getWeatherIcon(current.weather[0].main)}"></i>`
+        ]);
+
+        let currentWeather2 = combineElems([
+            createElem('p', 'Precipitation: ' + current.uvi + ' %'),
+            createElem('p', 'Humidity: ' + current.humidity + ' %'),
+            createElem('p', 'Wind: ' + current.wind_deg + ' km/hr')
+        ]);
 
         let dailyWeather = '';
-        $.each(daily, (key, val) => {
-            console.log('key: ', key);
-            console.log('val: ', val.weather[0].main);
-            dailyWeather += '<p class="cloudy"> ' + current.clouds + '</p >';
+        let day = new Date().getDay();
+        $.each(daily, (index, val) => {
+            let w = val.weather[0];
+            dailyWeather += combineElems([
+                '<div class="col-6 col-xs-6 col-md-1">',
+                createElem('p', days[day]),
+                `<i class="${getWeatherIcon(w.main)}"></i>`,
+                createElem('p', `${conv(val.temp.max)}° ${conv(val.temp.min)}°`),
+                '</div>'
+            ]);
+            day = day !== 0 ? day - 1 : 6;
         });
 
-
-        $('#test').append(currentWeather);
-        $('#test').append(dailyWeather);
+        $('#publicIp').text(`IP: ${geo.query}`);
+        $('#currentWeather1').append(currentWeather1);
+        $('#currentWeather2').append(currentWeather2);
+        $('#dailyWeather').append(dailyWeather);
     }
 
-    function getWClass() {
-        return "";
+    function getWeatherIcon(key) {
+        let w = {
+            Rain: 'fas fa-cloud-rain',
+            Clear: 'far fa-sun',
+            Clouds: 'fas fa-cloud-meatball',
+            Sun: 'far fa-sun',
+            CloudSun: 'fas fa-cloud-sun'
+        };
+        return w[key] ?? w['CloudSun'];
     }
 
-    loadAll();
+    $('#locateIpBtn').click(() => {
+        loadByIp($('#ip').val());
+    });
+
+    $('#loader').hide();
+
+    // loadInit();
+    /* I prefer to use this because it is more accurate */
+    loadByIp('');
 });
